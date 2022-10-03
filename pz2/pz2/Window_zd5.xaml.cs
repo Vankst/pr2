fe558af55a207423df3cd001f5cc8a7a92ccea52
Черепанов Максим Александрович <pr-20.102k-chm@NAVIATK.RU>
2022-10-03 08:51:36 +0300

    add z\d5

---
+++ pz2/pz2/Window_zd5.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pz2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window_zd5.xaml
+    /// </summary>
+    public partial class Window_zd5 : Window
+    {
+        public Window_zd5()
+        {
+            InitializeComponent();
+        }
+    }
+}
